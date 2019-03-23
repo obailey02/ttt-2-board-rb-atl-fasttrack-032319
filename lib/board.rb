13703f252f16120bfe4615714a48cfd3cb413c4a
Otis Bailey III <obailey04@yahoo.com>
2019-03-23 18:04:02 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,11 @@
 # Define the variable board below.
-row = ('0',"1","2")
-position 1: left 
-row("0")
-position 2: middle
-row("1")
-position 3: right
-row ('2')
+# row = ('0',"1","2")
+# position 1: left 
+# row("0")
+# position 2: middle
+# row("1")
+# position 3: right
+# row ('2')
 # move x to position 2, middle
 # row("1") = x 
 board = x 
